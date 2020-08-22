9b12048ca167d31d846f6fcdf24d04aa21116082
David Strauch <dmaster18@gmail.com>
2020-08-22 21:24:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Your code here
+
+function mapToNegativize(values) {
+  let new_values = [];
+  for (let i = 0; i < values.length; i++)
+    {
+      new_values.push(-1*values[i]);
+    }
+  return new_values;
+}
+
+function mapToNoChange(values) {
+  let new_values = [];
+  for (let i = 0; i < values.length; i++)
+    {
+      new_values.push(values[i]);
+    }
+  return new_values;
+}
+
+function mapToDouble(values) {
+  let new_values = [];
+  for (let i = 0; i < values.length; i++) {
+    new_values.push(values[i]*2)
+  }
+  return new_values;
+}
+
+function mapToSquare(values) {
+  let new_values = [];
+  for (let i = 0; i < values.length; i++) {
+    new_values.push(values[i]**2)
+  }
+  return new_values;
+}
+
+function reduceToTotal(values, startingPoint = 0) {
+  for (let i = 0; i < values.length; i++) {
+    startingPoint += values[i]
+  }
+  return startingPoint;
+}
+
+function reduceToAllTrue(values) {
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] == true) {
+      return true;
+    } else {
+      return false;
+      break;
+    }
+  }
+}
+
+function reduceToAnyTrue(values) {
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] == true) {
+      return true;
+      break;
+    } else {
+      return false;
+    }
+  }
+}
